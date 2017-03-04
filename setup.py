148e113323609b36811a0f89584c76ec843d70e8
Jesse Roux <roux.jesse@gmail.com>
2017-03-03 20:29:35 -0700

    Fixed setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+print(find_packages())
 
 setup(name="hamplify",
   version="0.1",
-  packages=["hamplify"],
+  packages=["hamplify", "hamplify.parsers", "scripts"],
   url="https://github.com/Kangaroux/hamplify",
   license="MIT",
   install_requires=["future"],
