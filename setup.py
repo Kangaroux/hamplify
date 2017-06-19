4d9ac6245348fd55f6ba18ded4267d4e24c98ee1
Jesse Roux <roux.jesse@gmail.com>
2017-06-19 01:25:56 -0400

    Update gitignore, setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(name="hamplify",
   version="1.0",
+  author="kangaroux",
+  author_email="roux.jesse@gmail.com",
   packages=["hamplify", "hamplify.parsers", "scripts"],
   url="https://github.com/Kangaroux/hamplify",
   license="MIT",
