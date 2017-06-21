7146519bbeeb779bad6636a4808c9e85adb70609
Jesse Roux <roux.jesse@gmail.com>
2017-06-21 12:34:18 -0400

    Update version to v1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="hamplify",
-  version="1.0.1",
+  version="1.1.0",
   author="kangaroux",
   author_email="roux.jesse@gmail.com",
   packages=["hamplify", "hamplify.parsers", "scripts"],
