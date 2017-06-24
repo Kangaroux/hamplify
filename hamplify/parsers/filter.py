25b3fe9e591b656415f80f3e30c5bbf6ae1ff239
Jesse Roux <roux.jesse@gmail.com>
2017-06-24 11:59:37 -0400

    Added synonyms for js and css filters

--- hamplify/parsers/filter.py
+++ hamplify/parsers/filter.py
@@ -9,11 +9,22 @@ class FilterParser(BaseParser):
     super(FilterParser, self).__init__(options)
 
   def parse(self, text):
-    if text == FILTER_PLAIN:
+    if self._matches(text, FILTER_PLAIN):
       return FilterPlain()
-    elif text == FILTER_JAVASCRIPT:
+    elif self._matches(text, FILTER_JAVASCRIPT):
       return FilterJavascript()
-    elif text == FILTER_CSS:
+    elif self._matches(text, FILTER_CSS):
       return FilterCSS()
 
     return Text(text)
+
+  def _matches(self, text, filters):
+    if not text.startswith(TOKEN_FILTER):
+      return False
+
+    text = text[len(TOKEN_FILTER):]
+
+    if type(filters) is str:
+      return text == filters
+    elif type(filters) is list or type(filters) is tuple:
+      return text in filters
